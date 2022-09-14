b06ce5289c54a60c9d5d93276748909dfd41f181
Mai Thanh Nhat Truong <30365921+mtntruong@users.noreply.github.com>
2022-09-14 16:29:00 +0900

    Routines

---
+++ test.py
@@ -0,0 +1,202 @@
+import os
+import cv2
+import torch
+import h5py
+import copy
+import time
+import argparse
+import numpy as np
+from model import RPCA_Net
+from util import write_EXR, luma, inv_luma, matlab_style_gauss2D
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--data',
+        default='hdm',
+        help='Choosing test data.',
+    )
+    return parser.parse_args()
+
+
+def load_pretrained(path, N_iter):
+    model = RPCA_Net(N_iter=N_iter)
+    model = model.cuda()
+    checkpoint = torch.load(path)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    model.eval()
+    return model
+
+
+def omega_creator(inp_channel):
+    Zth = 10
+    channel = copy.deepcopy(inp_channel)
+    channel = np.reshape(np.transpose(channel), (np.prod(channel.shape), 1))
+    omega = ((channel >= Zth) & (channel <= 255 - Zth)) * 1
+    return np.squeeze(omega)
+
+
+def data_mat_creator(inp_channel, channel_idx, ev):
+    channel = copy.deepcopy(inp_channel)
+    channel = np.transpose(channel).flatten()
+    rad_channel = (channel ** 2.2) / (2 ** ev)
+    return rad_channel
+
+
+def HDR_inference(model, inp_data, inp_omega):
+    data = torch.from_numpy(copy.deepcopy(inp_data).astype('float32'))
+    data = torch.unsqueeze(data, 0)
+    data = data.permute(0, 3, 2, 1).cuda()
+    omega = torch.from_numpy(copy.deepcopy(inp_omega).astype('float32'))
+    omega = torch.unsqueeze(omega, 0)
+    omega = omega.permute(0, 3, 2, 1).cuda()
+
+    start = time.time()
+    with torch.no_grad():
+        x_hat, hdr_prediction = model(data, omega)
+    end = time.time() - start
+
+    hdr_patch = hdr_prediction.cpu().numpy()
+    return hdr_patch, end
+
+
+def create_images(img_folder, out_folder, ckpt_path, N_iter, img_width, img_height):
+    # Load model
+    model = load_pretrained(ckpt_path, N_iter)
+    
+    # Patch sizes
+    psize = 128
+    plength = psize * psize
+
+    # Grid
+    w_grid = [0]; h_grid = [0]
+
+    while True:
+        if h_grid[-1] + psize < img_height:
+            h_grid.append(h_grid[-1] + psize)
+        if w_grid[-1] + psize < img_width:
+            w_grid.append(w_grid[-1] + psize)
+        else:
+            h_grid[-1] = img_height - psize
+            w_grid[-1] = img_width - psize
+            break
+    w_grid = np.array(w_grid, dtype=np.uint16)
+    h_grid = np.array(h_grid, dtype=np.uint16)
+
+    # HDR reconstruction
+    gt_img_name = sorted(os.listdir(os.path.join(img_folder, 'HDR')))
+    for exr in gt_img_name:
+        print(exr)
+        seq = exr[0:6]
+
+        # Storing final HDR image
+        HDR = np.float32(np.zeros((img_height, img_width, 3)))
+        
+        # Read exposure times
+        try:
+            with open(os.path.join(img_folder, 'EXP', seq + '.txt'), 'r') as f:
+               ev = [float(line.strip()) for line in f if line]
+        except:
+            print('No EXP info. Using default...')
+            ev = [0.0, 3.0, 6.0]
+
+        # Read image stack
+        img_stack = np.zeros((img_height, img_width, 3, 3))
+        for i in range(1, 4):
+            img = cv2.imread(os.path.join(img_folder, 'SEQ', seq + '_' + str(i).zfill(2) + '.tif'), -1)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img = np.float32(img)
+            img_stack[:, :, :, i-1] = img / 65535.0
+
+        # Read omega stack
+        omg_stack = np.zeros((img_height, img_width, 3, 3))
+        for i in range(1, 4):
+            omg = cv2.imread(os.path.join(img_folder, 'OMEGA', seq + '_' + str(i).zfill(2) + '.png'))
+            omg = cv2.cvtColor(omg, cv2.COLOR_BGR2RGB)
+            omg_stack[:, :, :, i - 1] = omg
+        omg_stack[omg_stack > 0] = 1
+
+        # Patch reconstruction
+        total = 0
+        i = 0; j = 0
+        while i < len(h_grid):
+            while j < len(w_grid):
+                RS = np.zeros((plength, 3)); GS = np.zeros((plength, 3)); BS = np.zeros((plength, 3))
+                RO = np.zeros((plength, 3)); GO = np.zeros((plength, 3)); BO = np.zeros((plength, 3))
+                h = h_grid[i]
+                w = w_grid[j]
+
+                for k in range(3):
+
+                    ldr = img_stack[h:h+psize, w:w+psize, :, k]
+                    ldr = np.squeeze(ldr)
+
+                    # For Omega
+                    omg = omg_stack[h:h+psize, w:w+psize, :, k]
+                    omg = np.squeeze(omg)
+                    RO[:, k] = np.reshape(np.transpose(omg[:, :, 0]), plength)
+                    GO[:, k] = np.reshape(np.transpose(omg[:, :, 1]), plength)
+                    BO[:, k] = np.reshape(np.transpose(omg[:, :, 2]), plength)
+
+                    # For SEQ
+                    RS[:, k] = data_mat_creator(ldr[:, :, 0], 0, ev[k])
+                    GS[:, k] = data_mat_creator(ldr[:, :, 1], 1, ev[k])
+                    BS[:, k] = data_mat_creator(ldr[:, :, 2], 2, ev[k])
+
+                omega = np.stack((RO, GO, BO), axis=2)
+                data = luma(np.stack((RS, GS, BS), axis=2)*65535.0)
+
+                # Patch inference and reshape
+                hdr_columns, etime = HDR_inference(model, data, omega)
+                total = total + etime
+                R = np.float32(np.transpose(np.reshape(np.transpose(hdr_columns[0, :]).flatten(), (psize, psize))))
+                G = np.float32(np.transpose(np.reshape(np.transpose(hdr_columns[1, :]).flatten(), (psize, psize))))
+                B = np.float32(np.transpose(np.reshape(np.transpose(hdr_columns[2, :]).flatten(), (psize, psize))))
+                hdr_patch = np.stack((R, G, B), axis=2)
+
+                HDR[h:h+psize, w:w+psize, :] = copy.deepcopy(hdr_patch)
+
+                j = j + 1
+            i = i + 1
+            j = 0
+
+        print('Cost ' + str(total) + 's')
+        
+        # Applying OMEGA_2
+        mask = np.squeeze(np.multiply(img_stack[:, :, :, 0], img_stack[:, :, :, 1]))
+        mask[mask < 1] = 0
+        HDR = inv_luma(HDR)/65535.0
+        HDR[mask == 1] = 1.0
+        img2 = cv2.imread(os.path.join(img_folder, 'SEQ', seq + '_02.tif'), -1)
+        img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+        img2 = np.float32(img2) / 65535.0
+        radiance = img2 ** 2.2 / (2 ** ev[1])
+        np.putmask(HDR, (img2 > 0.01) & (img2 < 0.99), radiance)
+        
+        # Final writing
+        write_EXR(os.path.join(out_folder, exr), HDR)
+
+
+if __name__ == '__main__':
+    opt_args = parse_args()
+
+    if opt_args.data == 'hdm':
+        if not os.path.exists('./HDM'):
+            os.mkdir('./HDM')
+        create_images('../Test_HDM',
+                      './HDM',
+                      './checkpoints/hdm.pth',
+                      N_iter=10, img_width=1820, img_height=980)
+
+    elif opt_args.data == 'hdrv':
+        if not os.path.exists('./HDRv'):
+            os.mkdir('./HDRv')
+        create_images('../Test_HDRv',
+                      './HDRv',
+                      './checkpoints/hdm.pth',
+                      N_iter=10, img_width=1280, img_height=720)
+
+    else:
+        print('Incorrect dataset.')
+    print('done')
---
+++ train.py
@@ -0,0 +1,111 @@
+import os
+import torch
+import argparse
+
+from util import DatasetDirectory, ExpandNetLoss
+from tqdm import tqdm
+from model import RPCA_Net
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-s',
+        '--save_path',
+        default='checkpoints',
+        help='Path for checkpointing.',
+    )
+    parser.add_argument(
+        '--resume',
+        help='Resume training from saved checkpoint(s).',
+    )
+    parser.add_argument(
+        '--checkpoint_freq',
+        type=int,
+        default=1,
+        help='Checkpoint model every x epochs.',
+    )
+    parser.add_argument(
+        '--loss_freq',
+        type=int,
+        default=20,
+        help='Report (average) loss every x iterations.',
+    )
+    parser.add_argument(
+        '--set_lr',
+        type=float,
+        default=-1,
+        help='Set new learning rate.',
+    )
+    return parser.parse_args()
+
+
+def train(opt):
+
+    torch.backends.cudnn.benchmark = True
+
+    train_path = '../Train_Pairs_X_tensor'
+    data_train = DatasetDirectory(train_path)
+    data_train_loader = torch.utils.data.DataLoader(data_train, batch_size=1, shuffle=True, num_workers=4)
+
+    model = RPCA_Net(N_iter=10)
+    model.cuda()
+    optimizer = torch.optim.Adam(model.parameters(), lr=3e-5)
+    loss = torch.nn.L1Loss()
+
+    if opt.resume is not None:
+        print('Resume training from' + opt.resume)
+        checkpoint = torch.load(opt.resume)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        epoch_0 = checkpoint['epoch'] + 1
+        model.train()
+    else:
+        print('Start training from scratch.')
+        epoch_0 = 1
+
+    if not opt.set_lr == -1:
+        for groups in optimizer.param_groups: groups['lr'] = opt.set_lr; break
+        print('New learning rate:', end=" ")
+        for groups in optimizer.param_groups: print(groups['lr']); break
+
+    if not os.path.exists(opt.save_path):
+        os.mkdir(opt.save_path)
+    else:
+        print('WARNING: save_path already exists. Checkpoints may be overwritten.')
+
+    avg_loss = 0
+    for epoch in tqdm(range(epoch_0, 10_001), desc='Training'):
+        for i, (ldr_in, omega, hdr_target) in enumerate(tqdm(data_train_loader, desc=f'Epoch {epoch}')):
+
+            ldr_in = ldr_in.cuda()
+            hdr_target = hdr_target.cuda()
+            omega = omega.cuda()
+
+            X_hat, X_hdr = model(ldr_in, omega)
+            total_loss = loss(X_hat, hdr_target) + loss(X_hdr, hdr_target[0, :, 1, :])
+
+            optimizer.zero_grad()
+            total_loss.backward()
+            optimizer.step()
+            avg_loss += total_loss.item()
+            if ((i + 1) % opt.loss_freq) == 0:
+                rep = (
+                    f'Epoch: {epoch:>5d}, '
+                    f'Iter: {i+1:>6d}, '
+                    f'Loss: {avg_loss/opt.loss_freq:>6.2e}'
+                )
+                tqdm.write(rep)
+                avg_loss = 0
+
+        if (epoch % opt.checkpoint_freq) == 0:
+            torch.save(
+                {'epoch': epoch, 'model_state_dict': model.state_dict(),
+                 'optimizer_state_dict': optimizer.state_dict()},
+                os.path.join(opt.save_path, f'epoch_{epoch}.pth')
+            )
+
+
+if __name__ == '__main__':
+    opt_args = parse_args()
+    train(opt_args)
